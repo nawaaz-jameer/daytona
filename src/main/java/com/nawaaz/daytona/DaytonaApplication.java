76af50c94c5be6575cfbd049c7e1e68768c9af3c
Nawaaz Jameer <nawaaz.jameer@gmail.com>
2024-08-28 22:15:21 +0400

    Project structure - NOT TESTED

---
+++ src/main/java/com/nawaaz/daytona/DaytonaApplication.java
@@ -0,0 +1,13 @@
+package com.nawaaz.daytona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaytonaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaytonaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nawaaz/daytona/DaytonaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nawaaz.daytona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaytonaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
